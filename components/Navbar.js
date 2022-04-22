533d39dc09ee1ba62340c2dc78a8ef47fb42eb39
Vikas <vikaskushwaha744@gmail.com>
2022-04-22 11:28:38 +0530

    Pages Content

--- components/Navbar.js
+++ components/Navbar.js
@@ -10,18 +10,18 @@ const Navbar = () => {
     <div>
       <div className="flex flex-col md:flex-row  md:justify-start justify-center py-2 shadow-md items-center">
         <div className={` mx-5 ${styles.logo}`}>
-           <Link href='/'><Image src='/footerlogo.png' width={200} height={40} alt=''/></Link></div>
+           <Link href='/'><a><Image src='/footerlogo.png' width={200} height={40} alt=''/></a></Link></div>
         <div className="nav">
-          <ul className="flex items-center space-x-12 font-semibold md:text-xl">
-           <Link href='/'><a><li>Vegetables&Fruits</li></a></Link>
-           <Link href='/'><a><li>Dairy Products</li></a></Link>
-           <Link href='/'><a> <li>Munchies</li></a></Link>
-            <Link href='/'><a><li>Sweet Tooth</li></a></Link>
+          <ul className="flex items-center space-x-8 font-semibold md:text-sm">
+           <Link href='/vegetables&Fruits'><a><li>Vegetables&Fruits</li></a></Link>
+           <Link href='/dairyproducts'><a><li>Dairy Products</li></a></Link>
+           <Link href='/munchies'><a> <li>Munchies</li></a></Link>
+            <Link href='/sweettooth'><a><li>Sweet Tooth</li></a></Link>
           </ul>
         </div>
       </div>
-          <div className="cart  absolute right-0 top-4 mx-5 my-3 ">
-            <FiShoppingCart className="text-3xl"/>
+          <div className="cart  absolute right-0 top-4 mx-5 my-1 ">
+           <Link href='/order'><a> <FiShoppingCart className="text-2xl"/></a></Link>
           </div>
     </div>
   );
